965d9c315cf3d2ef386c65e50962fdc8e0f00ecf
shiwei10 <shiwei10@staff.weibo.com>
2024-08-15 20:07:50 +0800

    feat: add auth logical

--- src/app/(registry)/login/page.tsx
+++ src/app/(registry)/login/page.tsx
@@ -32,7 +32,7 @@ export default function AuthenticationPage() {
 			</div>
 			<div className="container relative hidden h-[1000px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
 				<Link
-					href="/examples/authentication"
+					href="/login"
 					className={cn(
 						buttonVariants({ variant: "ghost" }),
 						"absolute right-4 top-4 md:right-8 md:top-8"
@@ -72,7 +72,7 @@ export default function AuthenticationPage() {
 					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 						<div className="flex flex-col space-y-2 text-center">
 							<h1 className="text-2xl font-semibold tracking-tight">
-								Login with account
+								Create an account
 							</h1>
 							<p className="text-sm text-muted-foreground">
 								Enter your email below to create your account

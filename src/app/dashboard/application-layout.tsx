5f7efce93265b9ccccc78eafc7f3c5110f5503f6
shiwei10 <shiwei10@staff.weibo.com>
2024-08-16 19:04:53 +0800

    style: format

--- src/app/dashboard/application-layout.tsx
+++ src/app/dashboard/application-layout.tsx
@@ -2,202 +2,202 @@
 
 import { Avatar } from '@/components/avatar'
 import {
-  Dropdown,
-  DropdownButton,
-  DropdownDivider,
-  DropdownItem,
-  DropdownLabel,
-  DropdownMenu,
+	Dropdown,
+	DropdownButton,
+	DropdownDivider,
+	DropdownItem,
+	DropdownLabel,
+	DropdownMenu,
 } from '@/components/dropdown'
-import { 
-  Navbar, 
-  NavbarItem, 
-  NavbarSection, 
-  NavbarSpacer, 
-  NavbarLabel, 
-  NavbarDivider 
+import {
+	Navbar,
+	NavbarItem,
+	NavbarSection,
+	NavbarSpacer,
+	NavbarLabel,
+	NavbarDivider
 } from '@/components/navbar'
 import {
-  Sidebar,
-  SidebarBody,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarHeading,
-  SidebarItem,
-  SidebarLabel,
-  SidebarSection,
-  SidebarSpacer,
+	Sidebar,
+	SidebarBody,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarHeading,
+	SidebarItem,
+	SidebarLabel,
+	SidebarSection,
+	SidebarSpacer,
 } from '@/components/sidebar'
 import { SidebarLayout } from '@/components/sidebar-layout'
 import { getEvents } from '@/data'
 import {
-  ArrowRightStartOnRectangleIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
-  Cog8ToothIcon,
-  LightBulbIcon,
-  PlusIcon,
-  ShieldCheckIcon,
-  UserCircleIcon,
-  UserIcon
+	ArrowRightStartOnRectangleIcon,
+	ChevronDownIcon,
+	ChevronUpIcon,
+	Cog8ToothIcon,
+	LightBulbIcon,
+	PlusIcon,
+	ShieldCheckIcon,
+	UserCircleIcon,
+	UserIcon
 } from '@heroicons/react/16/solid'
 import { usePathname } from 'next/navigation'
 import { StackedLayout } from '@/components/stacked-layout'
 import { InboxIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 function AccountDropdownMenu({ anchor }: { anchor: 'top start' | 'bottom end' }) {
-  return (
-    <DropdownMenu className="min-w-64" anchor={anchor}>
-      <DropdownItem href="#">
-        <UserCircleIcon />
-        <DropdownLabel>My account</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="#">
-        <ShieldCheckIcon />
-        <DropdownLabel>Privacy policy</DropdownLabel>
-      </DropdownItem>
-      <DropdownItem href="#">
-        <LightBulbIcon />
-        <DropdownLabel>Share feedback</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="#">
-        <ArrowRightStartOnRectangleIcon />
-        <DropdownLabel>Sign out</DropdownLabel>
-      </DropdownItem>
-    </DropdownMenu>
-  )
+	return (
+		<DropdownMenu className="min-w-64" anchor={anchor}>
+			<DropdownItem href="#">
+				<UserCircleIcon />
+				<DropdownLabel>My account</DropdownLabel>
+			</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem href="#">
+				<ShieldCheckIcon />
+				<DropdownLabel>Privacy policy</DropdownLabel>
+			</DropdownItem>
+			<DropdownItem href="#">
+				<LightBulbIcon />
+				<DropdownLabel>Share feedback</DropdownLabel>
+			</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem href="#">
+				<ArrowRightStartOnRectangleIcon />
+				<DropdownLabel>Sign out</DropdownLabel>
+			</DropdownItem>
+		</DropdownMenu>
+	)
 }
 
 function TeamDropdownMenu() {
-  return (
-    <DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
-      <DropdownItem href="/teams/1/settings">
-        <Cog8ToothIcon />
-        <DropdownLabel>Settings</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="/teams/1">
-        <Avatar slot="icon" src="/flags/mx.svg" />
-        <DropdownLabel>Tailwind Labs</DropdownLabel>
-      </DropdownItem>
-      <DropdownItem href="/teams/2">
-        <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
-        <DropdownLabel>Workcation</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="/teams/create">
-        <PlusIcon />
-        <DropdownLabel>New team&hellip;</DropdownLabel>
-      </DropdownItem>
-    </DropdownMenu>
-  )
+	return (
+		<DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
+			<DropdownItem href="/teams/1/settings">
+				<Cog8ToothIcon />
+				<DropdownLabel>Settings</DropdownLabel>
+			</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem href="/teams/1">
+				<Avatar slot="icon" src="/flags/mx.svg" />
+				<DropdownLabel>Tailwind Labs</DropdownLabel>
+			</DropdownItem>
+			<DropdownItem href="/teams/2">
+				<Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
+				<DropdownLabel>Workcation</DropdownLabel>
+			</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem href="/teams/create">
+				<PlusIcon />
+				<DropdownLabel>New team&hellip;</DropdownLabel>
+			</DropdownItem>
+		</DropdownMenu>
+	)
 }
 
 const navItems = [
-  { label: 'Home', url: '/dashboard' },
-  { label: 'Events', url: '/dashboard/events' },
-  { label: 'Orders', url: '/dashboard/orders' },
-  { label: 'Broadcasts', url: '/dashboard/broadcasts' },
+	{ label: 'Home', url: '/dashboard' },
+	{ label: 'Events', url: '/dashboard/events' },
+	{ label: 'Orders', url: '/dashboard/orders' },
+	{ label: 'Broadcasts', url: '/dashboard/broadcasts' },
 ]
 
 export function ApplicationLayout({
-  events,
-  children,
+	events,
+	children,
 }: {
-  events: Awaited<ReturnType<typeof getEvents>>
-  children: React.ReactNode
+	events: Awaited<ReturnType<typeof getEvents>>
+	children: React.ReactNode
 }) {
-  let pathname = usePathname()
+	let pathname = usePathname()
 
-  return (
-    <StackedLayout
-      navbar={
-        <Navbar>
-          <Dropdown>
-            <DropdownButton as={NavbarItem} className="max-lg:hidden">
-              <Avatar src="/flags/mx.svg" />
-              <NavbarLabel>真题网</NavbarLabel>
-              {/* <ChevronDownIcon /> */}
-            </DropdownButton>
-            <TeamDropdownMenu />
-          </Dropdown>
-          <NavbarDivider className="max-lg:hidden" />
-          <NavbarSection className="max-lg:hidden">
-            {navItems.map(({ label, url }) => (
-              <NavbarItem key={label} href={url}>
-                {label}
-              </NavbarItem>
-            ))}
-          </NavbarSection>
-          <NavbarSpacer />
-          <NavbarSection>
-            <NavbarItem href="/search" aria-label="Search">
-              <MagnifyingGlassIcon />
-            </NavbarItem>
-            <NavbarItem href="/inbox" aria-label="Inbox">
-              <InboxIcon />
-            </NavbarItem>
-            <Dropdown>
-              <DropdownButton as={NavbarItem}>
-                <Avatar src="/users/erica.jpg" square />
-              </DropdownButton>
-              <DropdownMenu className="min-w-64" anchor="bottom end">
-                <DropdownItem href="/my-profile">
-                  <UserIcon />
-                  <DropdownLabel>My profile</DropdownLabel>
-                </DropdownItem>
-                <DropdownItem href="/settings">
-                  <Cog8ToothIcon />
-                  <DropdownLabel>Settings</DropdownLabel>
-                </DropdownItem>
-                <DropdownDivider />
-                <DropdownItem href="/privacy-policy">
-                  <ShieldCheckIcon />
-                  <DropdownLabel>Privacy policy</DropdownLabel>
-                </DropdownItem>
-                <DropdownItem href="/share-feedback">
-                  <LightBulbIcon />
-                  <DropdownLabel>Share feedback</DropdownLabel>
-                </DropdownItem>
-                <DropdownDivider />
-                <DropdownItem href="/logout">
-                  <ArrowRightStartOnRectangleIcon />
-                  <DropdownLabel>Sign out</DropdownLabel>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </NavbarSection>
-        </Navbar>
-      }
-      sidebar={
-        <Sidebar>
-          <SidebarHeader>
-            <Dropdown>
-              <DropdownButton as={SidebarItem} className="lg:mb-2.5">
-                <Avatar src="/flags/mx.svg" />
-                <SidebarLabel>Tailwind Labs</SidebarLabel>
-                <ChevronDownIcon />
-              </DropdownButton>
-              <TeamDropdownMenu />
-            </Dropdown>
-          </SidebarHeader>
-          <SidebarBody>
-            <SidebarSection>
-              {navItems.map(({ label, url }) => (
-                <SidebarItem key={label} href={url}>
-                  {label}
-                </SidebarItem>
-              ))}
-            </SidebarSection>
-          </SidebarBody>
-        </Sidebar>
-      }
-    >
-      {children}
-    </StackedLayout>
-  )
+	return (
+		<StackedLayout
+			navbar={
+				<Navbar>
+					<Dropdown>
+						<DropdownButton as={NavbarItem} className="max-lg:hidden">
+							<Avatar src="/flags/mx.svg" />
+							<NavbarLabel>真题网</NavbarLabel>
+							{/* <ChevronDownIcon /> */}
+						</DropdownButton>
+						<TeamDropdownMenu />
+					</Dropdown>
+					<NavbarDivider className="max-lg:hidden" />
+					<NavbarSection className="max-lg:hidden">
+						{navItems.map(({ label, url }) => (
+							<NavbarItem key={label} href={url}>
+								{label}
+							</NavbarItem>
+						))}
+					</NavbarSection>
+					<NavbarSpacer />
+					<NavbarSection>
+						<NavbarItem href="/search" aria-label="Search">
+							<MagnifyingGlassIcon />
+						</NavbarItem>
+						<NavbarItem href="/inbox" aria-label="Inbox">
+							<InboxIcon />
+						</NavbarItem>
+						<Dropdown>
+							<DropdownButton as={NavbarItem}>
+								<Avatar src="/users/erica.jpg" square />
+							</DropdownButton>
+							<DropdownMenu className="min-w-64" anchor="bottom end">
+								<DropdownItem href="/my-profile">
+									<UserIcon />
+									<DropdownLabel>My profile</DropdownLabel>
+								</DropdownItem>
+								<DropdownItem href="/settings">
+									<Cog8ToothIcon />
+									<DropdownLabel>Settings</DropdownLabel>
+								</DropdownItem>
+								<DropdownDivider />
+								<DropdownItem href="/privacy-policy">
+									<ShieldCheckIcon />
+									<DropdownLabel>Privacy policy</DropdownLabel>
+								</DropdownItem>
+								<DropdownItem href="/share-feedback">
+									<LightBulbIcon />
+									<DropdownLabel>Share feedback</DropdownLabel>
+								</DropdownItem>
+								<DropdownDivider />
+								<DropdownItem href="/logout">
+									<ArrowRightStartOnRectangleIcon />
+									<DropdownLabel>Sign out</DropdownLabel>
+								</DropdownItem>
+							</DropdownMenu>
+						</Dropdown>
+					</NavbarSection>
+				</Navbar>
+			}
+			sidebar={
+				<Sidebar>
+					<SidebarHeader>
+						<Dropdown>
+							<DropdownButton as={SidebarItem} className="lg:mb-2.5">
+								<Avatar src="/flags/mx.svg" />
+								<SidebarLabel>Tailwind Labs</SidebarLabel>
+								<ChevronDownIcon />
+							</DropdownButton>
+							<TeamDropdownMenu />
+						</Dropdown>
+					</SidebarHeader>
+					<SidebarBody>
+						<SidebarSection>
+							{navItems.map(({ label, url }) => (
+								<SidebarItem key={label} href={url}>
+									{label}
+								</SidebarItem>
+							))}
+						</SidebarSection>
+					</SidebarBody>
+				</Sidebar>
+			}
+		>
+			{children}
+		</StackedLayout>
+	)
 }
 
 
